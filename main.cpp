2d0f05229436821663d864fba47f86bd26e00fab
Carlos R. Arias <arias@spu.edu>
2020-10-07 10:00:31 -0700

    It counts letters

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::getline;
+
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	string line;
+	cout << "Input a line: ";
+	getline(cin, line);
+	size_t letterCount = 0;
+	for (int i = 0; i < line.size(); ++i) {
+		if (line.at(i) != ' ') // subscript operator is OVERLOADED for class string!
+			letterCount++;
+	}
+	cout << "I counted " << letterCount << " letters" << endl;
+
+
 	return 0;
 }
